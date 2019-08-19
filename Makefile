79df9620f6f1a2caeaec93fd2ac36e72e88cb77d
Masakazu Matsumoto <vitroid@gmail.com>
2019-08-19 23:20:50 +0900

    v1.0

--- Makefile
+++ Makefile
@@ -1,10 +1,26 @@
+PKGNAME=genice-extra
+all:
+	echo Hello.
 .DELETE_ON_ERROR:
+
+test-deploy: build
+	twine upload -r pypitest dist/*
+test-install:
+	pip install --index-url https://test.pypi.org/simple/ $(PKGNAME)
+
+install: check
+	./setup.py install
+uninstall:
+	pip uninstall $(PKGNAME)
+build: README.md ./setup.py
+	./setup.py sdist bdist_wheel
+
+deploy: build
+	twine upload dist/*
 check:
 	./setup.py check
-install:
-	./setup.py install
-pypi: check
-	./setup.py sdist bdist_wheel upload
+
+
 clean:
 	-rm $(ALL) *~ */*~
 	-rm -rf build dist *.egg-info
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_desc = "".join(open("README.md").readlines())
 
 setup(
     name='genice_extra',
-    version='0.2',
+    version='1.0',
     description='Installs all extra plugins for GenIce.',
     long_description=long_desc,
     long_description_content_type="text/markdown",
@@ -22,9 +22,12 @@ setup(
     author_email='vitroid@gmail.com',
     url='https://github.com/vitroid/genice-extra/',
     keywords=['genice'],
-    install_requires=['genice-vpython',
-                      'genice-rdf',
-                      'genice-svg',
+    install_requires=['genice-vpython>=0.4',
+                      'genice-rdf>=0.3',
+                      'genice-svg>=0.4',
+                      'genice-twist>=0.1',
+                      'genice-cif>=0.2',
+                      'GenIce>=1.0rc5',
                       ],
 
     license='MIT',
